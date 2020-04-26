13f66e8809c8893dbaa6290dadc473e7de7b8f20
charconstpointer <attwistingnether@gmail.com>
2020-04-26 09:46:27 +0200

    CW5--------------------

---
+++ APBD3.API/Exceptions/StudiesNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APBD3.API.Exceptions
+{
+    public class StudiesNotFoundException : Exception
+    {
+        public StudiesNotFoundException(string message):base(message)
+        {
+            
+        }
+    }
+}
